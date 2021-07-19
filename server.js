9116f8f2ff72d970f8662b79f984d88b1f66b2a5
Krish Bajaj <bmohit68@gmail.com>
2021-07-19 23:47:57 +0530

    cors trial

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
-const cors = require('cors');
-// Cors 
+// const cors = require('cors');
+// // Cors 
 
 
-const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300','http://localhost:5500']
-  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
-app.use(cors(corsOptions))
-app.use(express.static('public'));
+// const corsOptions = {
+//   origin: ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300','http://localhost:5500']
+//   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+// }
+// app.use(cors(corsOptions))
+// app.use(express.static('public'));
+var express = require('express')
+var cors = require('cors')
+var app = express()
+ 
+app.use(cors())
+ 
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+ 
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
 
 const connectDB = require('./config/db');
 connectDB();
