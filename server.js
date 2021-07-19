a341537cd82559caade888151adda38d55ca670b
Krish Bajaj <bmohit68@gmail.com>
2021-07-20 00:15:55 +0530

    bug fixes

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
-// const cors = require('cors');
-// // Cors 
+const cors = require('cors');
+// Cors 
 
 
-// const corsOptions = {
-//   origin: ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300','http://localhost:5500']
-//   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-// }
-// app.use(cors(corsOptions))
-// app.use(express.static('public'));
-var express = require('express')
-var cors = require('cors')
-var app = express()
- 
-var corsOptions = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+const corsOptions = {
+  origin: ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300','http://localhost:5500']
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 }
+app.use(cors(corsOptions))
+app.use(express.static('public'));
+// var express = require('express')
+// var cors = require('cors')
+// var app = express()
  
-app.get('/products/:id', cors(corsOptions), function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for only example.com.'})
-})
+// var corsOptions = {
+//   origin: 'http://localhost:3000',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
  
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
+// app.get('/products/:id', cors(corsOptions), function (req, res, next) {
+//   res.json({msg: 'This is CORS-enabled for only example.com.'})
+// })
+ 
+// app.listen(80, function () {
+//   console.log('CORS-enabled web server listening on port 80')
+// })
 
 const connectDB = require('./config/db');
 connectDB();
