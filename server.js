01739390556fdd97ee2a6ea4d8c47500a8f5f1e2
Krish Bajaj <bmohit68@gmail.com>
2021-07-22 22:34:27 +0530

    cors final maybe

--- server.js
+++ server.js
@@ -3,32 +3,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 const cors = require('cors');
-// Cors 
-
-
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300','http://localhost:5500']
+  origin: "*",
   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 }
 app.use(cors(corsOptions))
-app.use(express.static('public'));
-// var express = require('express')
-// var cors = require('cors')
-// var app = express()
- 
-// var corsOptions = {
-//   origin: 'http://localhost:3000',
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
- 
-// app.get('/products/:id', cors(corsOptions), function (req, res, next) {
-//   res.json({msg: 'This is CORS-enabled for only example.com.'})
-// })
- 
-// app.listen(80, function () {
-//   console.log('CORS-enabled web server listening on port 80')
-// })
 
+app.use(express.static('public'));
 const connectDB = require('./config/db');
 connectDB();
 app.use(express.json());
